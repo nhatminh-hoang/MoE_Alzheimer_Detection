895bab2c2f9125b46b62892af52f2ff2bddfd7a7
nhatminh-hoang <hminhnhat.ai@gmail.com>
2025-02-13 10:28:02 +0700

    Update training script to include CNN, LSTM, and BiLSTM models; remove redundant Transformer training commands

--- sh_framework_hpc.sh
+++ sh_framework_hpc.sh
@@ -11,11 +11,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=21013299@st.phenikaa-uni.edu.vn
 
-pip install -r requirements.txt
-
+python train.py --data_name ADReSS2020 --wave_type full --feature_type MFCC --model CNN --epochs 1000 --batch_size 256 --lr 1e-3 --hidden_size 128 --dropout 0.25 --early_stop no
+python train.py --data_name ADReSS2020 --wave_type full --feature_type MFCC --model LSTM --epochs 1000 --batch_size 256 --lr 1e-3 --hidden_size 128 --dropout 0.25 --early_stop no
+python train.py --data_name ADReSS2020 --wave_type full --feature_type MFCC --model BiLSTM --epochs 1000 --batch_size 256 --lr 1e-3 --hidden_size 128 --dropout 0.25 --early_stop no
 python train.py --data_name ADReSS2020 --wave_type full --feature_type MFCC --model Transformer --epochs 1000 --batch_size 256 --lr 1e-3 --hidden_size 128 --dropout 0.25 --early_stop no
-python train.py --data_name ADReSS2020 --wave_type chunk --feature_type MFCC --model Transformer --epochs 1000 --batch_size 256 --lr 1e-3 --hidden_size 32 --dropout 0.25 --early_stop no
-python train.py --data_name ADReSS2020 --wave_type chunk --feature_type MFCC --model Transformer --epochs 1000 --batch_size 256 --lr 1e-3 --hidden_size 64 --dropout 0.25 --early_stop no
-python train.py --data_name ADReSS2020 --wave_type chunk --feature_type MFCC --model Transformer --epochs 1000 --batch_size 256 --lr 1e-3 --hidden_size 128 --dropout 0.25 --early_stop no
-python train.py --data_name ADReSS2020 --wave_type chunk --feature_type MFCC --model Transformer --epochs 1000 --batch_size 256 --lr 1e-3 --hidden_size 256 --dropout 0.25 --early_stop no
-python train.py --data_name ADReSS2020 --wave_type chunk --feature_type MFCC --model Transformer --epochs 1000 --batch_size 256 --lr 1e-3 --hidden_size 512 --dropout 0.25 --early_stop no
